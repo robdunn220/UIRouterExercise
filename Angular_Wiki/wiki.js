53312bbe827f479ad49615c3298b35b841f2f1ba
Robert Dunn <robertdunn220@gmail.com>
2016-11-14 21:15:28 -0500

    Almost finished Angular_Wiki

--- Angular_Wiki/wiki.js
+++ Angular_Wiki/wiki.js
@@ -1,29 +1,46 @@
 var app = angular.module('wikis', ['ui.router']);
 
-var pages = [{name: 'Curling', descr: 'Curling is really fun, and cool, eh?'},
-            {name: 'Che', descr: 'Che sucks, eh?'}];
+function WikiPage(title, content) {
+  this.title = title;
+  this.content = content;
+}
 
+var pages = {
+  Python: new WikiPage('Python', 'Python is a fun to use programming language. It is great for beginners.'),
+  HTML: new WikiPage('HTML', 'HTML is the markup language used for making pages on the world wide web.')
+  };
 
-app.config(function($stateProvider, $stateParams) {
+app.controller('PageViewController', function($scope, $stateParams) {
+  var nombreDePagina = $stateParams.page_name;
+  $scope.page = pages[nombreDePagina];
+});
+
+app.controller('PageEditController', function($scope, $stateParams) {
+  var pageName = $stateParams.page_name;
+  $scope.pageEdit = pages[pageName];
+
+  $scope.saveEdit = function(newContent) {
+    pages[pageName].content = newContent;
+  };
+});
+
+app.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
   .state({
     name: 'home',
     url: '/',
-    templateUrl: '/home.html',
+    templateUrl: '/home.html'
   })
   .state({
     name: 'page_view',
     url: '/{page_name}',
-    templateUrl: '/page_view.html',
+    templateUrl: 'page_view.html',
     controller: 'PageViewController'
+  })
+  .state({
+    name: 'page_edit',
+    url: '/{page_name}/edit',
+    templateUrl: 'page_edit.html',
+    controller: 'PageEditController'
   });
-});
-
-app.controller('PageViewController', function($scope, $stateParams) {
-  var page_name = $stateParams.page_name;
-  for (var p = 0; p < pages.length; p++) {
-    if (pages[p].name === pageName) {
-      $scope.pageContent = pages[p];
-    }
-  }
 });
